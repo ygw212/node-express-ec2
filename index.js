bcbe83d4f4c60d6a32b0571becd8976af6e15cb1
ygw212 <wangygca@gmail.com>
2024-04-07 20:10:26 -0700

    add Docker file

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const app = express();
 app.listen(5001,()=>console.log('API running on port 5001'));
 
 app.get('/',(req,res)=>res.json('My API running'))
+
+app.get("/me", (req, res) => res.json("This is my page"));
